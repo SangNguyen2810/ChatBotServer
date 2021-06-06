fbe1aed1acb367cd715ae10a7844e13fb8ec12d6
Sang Nguyen <sangnq3@vng.com.vn>
2021-06-06 14:12:08 +0700

    [Test for conflict merges]

--- app.js
+++ app.js
@@ -11,16 +11,7 @@ import cors from "cors";
 const startApp = () => {
   const app = express();
   const port = process.env.PORT || 8000;
-  const corsOptions = {
-    origin: "http://localhost:3000",
-    optionSuccessStatus: 200,
-  }
-  app.use(cors(corsOptions));
-  app.get("/hello", (req, res) => res.send("hello world from cules coding"));
-  app.listen(port, () => console.log(`Server is running on ${port}`));
-  app.use(cookieParser());
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+
   
 
   //Adding authentication JWT for every route, except login/register
